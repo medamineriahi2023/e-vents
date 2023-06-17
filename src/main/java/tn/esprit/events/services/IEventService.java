328abd5c7a8f4aee77f76cdce54e8cc5a830c6b6
Safwen Soker <safwen.soker@pyxis.com.tn>
2023-06-17 09:46:01 +0100

    feat: add event service abstract functions

--- src/main/java/tn/esprit/events/services/IEventService.java
+++ src/main/java/tn/esprit/events/services/IEventService.java
@@ -2,9 +2,23 @@ package tn.esprit.events.services;
 
 import tn.esprit.events.dtos.CommentDto;
 import tn.esprit.events.dtos.EventDto;
+import tn.esprit.events.entities.Event;
 import tn.esprit.events.services.abstracts.ICrudService;
 
+import java.util.Date;
+import java.util.List;
+
+
 public interface IEventService extends ICrudService<EventDto> {
+
+    List<Event> getAllEvents();
+    Event createEvent(Event event);
+    Event updateEvent(long id, Event event);
+
+    Event getEventById(long id);
+    Event archiveEvent(long id);
+    Event rescheduleEvent(long id, Date dateDebut, Date dateFin);
+    Event createOnSiteEvent(Event event);
 
 
 }
